5634956e64b8419e5adff09166f738c3898a8758
kostejnv <v.kostejn@seznam.cz>
2022-08-15 08:58:56 +0200

    debug false

--- o_startlist_creator/o_startlist_creator/settings.py
+++ o_startlist_creator/o_startlist_creator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kllo=q2@xj=&k%74fw+xr!v&r@ijg2boj2c1fpn7uht)ha*m+q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
