aa62d69fce6f0a7c0f902c0ccc4605a98c047b47
kostejnv <v.kostejn@seznam.cz>
2023-05-01 17:43:40 +0200

    double stage solving

--- o_startlist_creator/o_startlist_creator/logic/solver.py
+++ o_startlist_creator/o_startlist_creator/logic/solver.py
@@ -1,17 +1,19 @@
 from .event import Event
 from .validator import CoursesValidator
-from .solvers.minizinc_solver import Minizinc
+from .solvers.minizinc_solver import MinizincMain, Minizinc2
 import datetime
 
-SOLVER_TIMEOUT = 600
+SOLVER_TIMEOUT = 300
 
 class Solver:
     def __init__(self) -> None:
         pass
 
     def solve(self, event:Event) -> None:
-        solver = Minizinc(timeout=datetime.timedelta(seconds=SOLVER_TIMEOUT))
+        solver = MinizincMain(timeout=datetime.timedelta(seconds=SOLVER_TIMEOUT))
         individual_cats, schedule_length = solver.solve(event)
+        solver = Minizinc2(timeout=datetime.timedelta(seconds=SOLVER_TIMEOUT))
+        individual_cats, schedule_length = solver.solve(event, schedule_length)
         event = self.__merge_solved_cats_to_event(individual_cats, event)
         if not CoursesValidator(event, schedule_length).validate_schedule():
             raise "INTERNAL ERROR: Returned scheduled is not correct"
